cf762ff58dd34b09f78c5fd449abd610504ef01f
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2025-11-13 21:52:24 +0800

    fix: debug 命令输出去重

--- common/src/main/java/com/hexagram2021/chromosomelib/common/command/ChromosomeLibCommand.java
+++ common/src/main/java/com/hexagram2021/chromosomelib/common/command/ChromosomeLibCommand.java
@@ -1,12 +1,13 @@
 package com.hexagram2021.chromosomelib.common.command;
 
 import com.hexagram2021.chromosomelib.common.chromosome.Chromosome;
-import com.hexagram2021.chromosomelib.common.chromosome.ChromosomeInstance;
 import com.hexagram2021.chromosomelib.common.entity.IChromosomeCarrier;
 import com.hexagram2021.chromosomelib.common.gene.Gene;
+import com.hexagram2021.chromosomelib.registry.AbstractRegisterEntry;
 import com.hexagram2021.chromosomelib.registry.CLRegistries;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
@@ -21,6 +22,7 @@ import org.jetbrains.annotations.ApiStatus;
 
 import javax.annotation.Nullable;
 import java.util.Comparator;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.ToIntFunction;
 
@@ -71,17 +73,6 @@ public final class ChromosomeLibCommand {
 		});
 	}
 
-	private static final Comparator<ChromosomeInstance> chromosomeInstanceComparator = (o1, o2) -> {
-		Holder<Chromosome> c1 = o1.chromosome();
-		Holder<Chromosome> c2 = o2.chromosome();
-		int i1 = c1.value().index();
-		int i2 = c2.value().index();
-		if(i1 == i2) {
-			return chromosome2Loc(c1).compareTo(chromosome2Loc(c2));
-		}
-		return Integer.compare(i1, i2);
-	};
-
 	@SuppressWarnings("unchecked")
 	private static int show(@Nullable ServerPlayer player, @Nullable Entity entity) {
 		if(chromosomeRegistry == null) {
@@ -90,19 +81,23 @@ public final class ChromosomeLibCommand {
 		if(geneRegistry == null) {
 			geneRegistry = (Registry<Gene>) Objects.requireNonNull(BuiltInRegistries.REGISTRY.get(CLRegistries.GENES.location()));
 		}
+		Map<Holder<Chromosome>, Object2IntMap<Holder<Gene>>> toShow = AbstractRegisterEntry.newHolderTreeMap();
 		if(player != null && entity instanceof IChromosomeCarrier carrier) {
 			ToIntFunction<Holder<Gene>> activeGenes = carrier.chromosomelib$getActiveGenes();
-			StringBuilder builder = new StringBuilder();
-			carrier.chromosomelib$getChromosomes().stream().sorted(chromosomeInstanceComparator).forEach(chromosomeInstance -> {
-				builder.append(chromosome2Loc(chromosomeInstance.chromosome())).append(":\n");
-				chromosomeInstance.geneLocusInstances().values().stream()
-						.sorted(Comparator.comparingInt(geneLocusInstance -> geneLocusInstance.index(chromosomeInstance.type())))
-						.forEach(geneLocusInstance -> builder.append("  - ")
-								.append(gene2Loc(geneLocusInstance.gene()))
-								.append(": ")
-								.append(activeGenes.applyAsInt(geneLocusInstance.gene()))
-								.append('\n'));
+			carrier.chromosomelib$getChromosomes().forEach(chromosomeInstance -> {
+				Object2IntMap<Holder<Gene>> chromosomeGenes = toShow.computeIfAbsent(chromosomeInstance.chromosome(), ignored -> AbstractRegisterEntry.newHolderObject2IntTreeMap());
+				chromosomeInstance.geneLocusInstances().forEach((index, geneLocusInstance) -> chromosomeGenes.put(geneLocusInstance.gene(), activeGenes.applyAsInt(geneLocusInstance.gene())));
 			});
+			StringBuilder builder = new StringBuilder();
+			toShow.keySet().stream().sorted(Comparator.comparing(ChromosomeLibCommand::chromosome2Loc)).forEach(chromosome -> {
+				builder.append(chromosome2Loc(chromosome)).append(":\n");
+				toShow.get(chromosome).keySet().forEach(gene -> builder.append("  - ")
+						.append(gene2Loc(gene))
+						.append(": ")
+						.append(activeGenes.applyAsInt(gene))
+						.append('\n'));
+			});
+
 			player.sendSystemMessage(Component.literal(builder.toString()));
 		}
 		return Command.SINGLE_SUCCESS;
